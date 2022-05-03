b9fb0ac09dcd087e35afb27003685b503ae7f6c8
Douglas B. de Oliveira <douglasboliveira@gmail.com>
2022-05-03 12:05:15 -0300

    add dunder e os library

--- hello.py
+++ hello.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas
 
-#isso eh um comentario
-print("Hello, World!")
+Dependendo da lingua configurada no ambiente o programa exibira
+a mensagem com o idioma correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execucao:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+
+import os
+
+__version__="0.0.1"
+__author__="Douglas B. de Oliveira"
+__license__="Unlicense"
+
+
+current_language = os.getenv("LANG")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
